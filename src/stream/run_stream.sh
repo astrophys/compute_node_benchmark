f85fa2958b2bbf5553e2eb7ee3736bc63f667af6
Ali Snedden <ali.snedden@nationwidechildrens.org>
2019-06-07 13:44:20 -0400

    Added John's new run_stream.sh, it tries to keep all the cores on a single NUMA node

--- src/stream/run_stream.sh
+++ src/stream/run_stream.sh
@@ -6,22 +6,32 @@
 #         egrep 'physical id.*: 0'| \
 #         perl -ane 'print ("$F[2]\n");'`
 set -e
-COREIDS=`grep -P 'processor[\t ]' /proc/cpuinfo | cut -d: -f2 | tr -d ' '`
-COUNT=1
 STREAM=$1
+THREADS=1
+NODES=
 
-for CORE in $COREIDS; do 
-   if [ "$CORES" ] ; then
-       CORES="$CORES,$CORE";
-   else
-       CORES=$CORE
-   fi
-   echo -n "$COUNT threads: "
-   export OMP_NUM_THREADS=$COUNT
-   taskset -c $CORES $STREAM | \
-       egrep '^(Copy|Scale|Add|Triad):' | \
-       awk '{print $1 " " $2}' | \
-       tr '\n' ' '
-    echo
-    COUNT=`expr $COUNT + 1`
+SOCKETIDS=`lscpu -e | grep -v SOCKET | tr -s ' ' | cut -d' ' -f3 | sort -u`
+for SOCKET in $SOCKETIDS ; do
+    NODEIDS=`lscpu -e | grep -v SOCKET | tr -s ' ' | cut -d' ' -f2,3 | egrep $SOCKET'$' | cut -d' ' -f1 | sort -u`
+    for NODE in $NODEIDS ; do
+	if [ "$NODES" ] ; then
+	    NODES="$NODES,$NODE"
+        else
+            NODES=$NODE
+        fi
+	CPUIDS=`lscpu -e | grep -v SOCKET | tr -s ' ' | cut -d' ' -f1,2,3 | egrep "$NODE $SOCKET\$" | cut -d' ' -f1 | sort -nu`
+	for CPU in $CPUIDS ; do
+            echo -n THREADS $THREADS NODES $NODES CPU $CPU" "
+	    OMP_NUM_THREADS=$THREADS numactl --membind=$NODES --cpunodebind=$NODES ${STREAM} | \
+                egrep '^(Copy|Scale|Add|Triad):' | \
+                awk '{print $1 " " $2}' | \
+                tr '\n' ' '
+            echo
+	    THREADS=`expr $THREADS + 1`
+        done
+	
+    done
 done
+
+
+exit 0

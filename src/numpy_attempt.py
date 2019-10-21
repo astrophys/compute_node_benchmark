371891423d99a09bb15a0cc88bba0314012178c0
Ali Snedden <ali.snedden@nationwidechildrens.org>
2019-10-21 14:01:11 -0400

    numpy_attempt.py : tightened things up a lot

--- src/numpy_attempt.py
+++ src/numpy_attempt.py
@@ -1,22 +1,18 @@
 # Author : Ali Snedden
 # Date   : 5/15/19
 # License: MIT
-# Purpose: 
+# Purpose:
 #
 # Notes :
 #
 # Questions:
 #
-# References :  
+# References :
 #
 import time
 import sys
-import os
-import glob
 import numpy as np
-import subprocess
-import shutil
-import bisect
+import random
 from error import exit_with_error
 from functions import parse_run_time
 from functions import get_core_ids
@@ -34,9 +30,7 @@ def print_help(Arg):
     FUTURE:
     """
     sys.stdout.write(
-            "USAGE : ./src/driver.py [options] /abs/path/to/workspace  /abs/path/to/ref\n"
-            "   [options] = 'all' : Builds data, runs all tests\n"
-            "             = 'build_mat_mult_data' : Builds matrix_multiply data \n"
+            "USAGE : ./src/numpy_attempt.py \n"
             "   NOTES : \n"
             )
     sys.exit(Arg)
@@ -59,58 +53,25 @@ def main():
     nArg = len(sys.argv)
     if(nArg == 2 and (sys.argv[1][0:3] == "--h" or sys.argv[1][0:2] == "-h")):
         print_help(0)
-    elif(nArg != 3):
+    elif(nArg != 1):
         print_help(1)
-    startTime = time.time()
-    print("Start Time : {}".format(time.strftime("%a, %d %b %Y %H:%M:%S ",
-                                   time.localtime())))
-    print("Logging run output to driver.log\n\n")
     ### Variables ###
-    options    = sys.argv[1]
-    workPath   = sys.argv[2]           # Path where all the output/work will be saved.
-    ompNumThreadsL = [1,2,5,20]        # Cores used in OMP tasks
-    matrixSizeL = [10000]     # outer dim of mats to run matrix_multiply on
-    #matrixSizeL = [2000,3000,5000]     # outer dim of mats to run matrix_multiply on
-    nTrials     = 3                     # number of trials to test,get stdev and mean
-    shortNTrials= 1                # shortened num of trials to test,get stdev and mean
-    # Create work path dir if doesn't exist
-    if(not os.path.isdir(workPath)):
-        os.mkdir(workPath)
+    matrixSize = 5000     # outer dim of mats to run matrix_multiply on
+    N = 50       # shortened num of trials to test,get stdev and mean
 
-    if(options != 'all' and options != 'build_mat_mult_data'):
-        exit_with_error("ERROR!!! {} is invalid option\n".format(options))
-
-
-    ######## Run Tests ########
-    if(options == 'all' or options == 'build_mat_mult_data'):
-        nThread = 1
-        #print("Building data for matrix_multiply (time to run is for numpy's matrix mult.: ")
-        #print("--------------------------------------------------------")
-        #print(" {:<10} | {:<12} | {:<15} | {:<15}".format("Size", "OMP_Threads", "mean",
-        #      "stdev"))
-        #print("--------------------------------------------------------")
-        ### Create directory structure in data
-        outDirPrefix = "{}/data/matrix".format(workPath)
-        if(not os.path.isdir(outDirPrefix)):
-            os.mkdir(outDirPrefix)
-
-        for size in matrixSizeL:
-            outDir = "{}/{}".format(outDirPrefix,size)
-            if(not os.path.isdir(outDir)):
-                os.mkdir(outDir)
-            runTimeV = np.zeros([shortNTrials])
-            for tIdx in range(shortNTrials):   ### change to shortNTrials
-                cmd =  ("python3 src/matrix/matrix_generator.py {} 10000 "
-                             "10000 {} {}".format(size, size, outDir))
-                output = "{}\n".format(cmd)
-                output = output + subprocess.getoutput(cmd)
-                runTime = parse_run_time(output,workPath) # Run time
-                runTimeV[tIdx]= runTime
-            print(" {:<10} | {:<12} | {:<15.4f} | {:<15.4f}".format(size, nThread,
-                      np.mean(runTimeV), np.std(runTimeV)))
-            print("--------------------------------------------------------")
-
-
+    random.seed(42)
+    np.random.seed(42)
+    Ax = matrixSize
+    Ay = 10000
+    Bx = 10000
+    By = matrixSize
+    A=np.random.rand(Ax,Ay)
+    B=np.random.rand(Bx,By)
+    print("Matrix Size = [{} {}] x [{} {}] = multiplied {} times ".format(Ax,Ay,Bx,By,N))
+    npStartTime = time.time()
+    for i in range(N):
+        AB = np.dot(A,B)
+    print("Run time : {:.4f} s".format((time.time() - npStartTime)))
 
 
 if __name__ == "__main__":
